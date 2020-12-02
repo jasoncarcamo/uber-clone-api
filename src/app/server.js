5ec5e6e0be9f59b5a34f25c6dc88a0d8c6f2a75b
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-12-02 12:34:13 -0500

    Added socket.io

--- src/app/server.js
+++ src/app/server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const {PORT, DATABASE_URL} = require("../../config");
 const knex = require("knex");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 const db = knex({
     client: 'pg',
@@ -8,6 +10,7 @@ const db = knex({
 });
 
 app.set("db", db);
+app.set("io", io);
 
 app.listen( PORT, ()=>{
     console.log(`App working. Listening on port: ${PORT}`);
