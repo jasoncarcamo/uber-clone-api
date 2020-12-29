42dc1b31d5e3a1b9d99ee5f27e5c3ce6498a3ec0
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-12-28 18:31:12 -0800

    Added time data relevant rows

--- migrations/004.do.create_members_table.sql
+++ migrations/004.do.create_members_table.sql
@@ -3,5 +3,8 @@ CREATE TABLE members (
     days_work TEXT [] DEFAULT '{}' NOT NULL,
     upcoming_days_off TEXT [] DEFAULT '{}' NOT NULL,
     work_address TEXT NOT NULL,
+    preferred_home_arrival TEXT NOT NULL,
+    time_work_starts TEXT NOT NULL,
+    time_off_work TEXT NOT NULL,
     passenger_id BIGINT REFERENCES passengers(id) ON DELETE CASCADE NOT NULL
 );
