e14705267cc9fc54dfd6799166ed7b8972ee9735
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2021-02-08 10:57:50 -0800

    Added route endppints for /api/trip/:id

--- src/routes/TripsRoutes/TripsRouter/TripsRouter.js
+++ src/routes/TripsRoutes/TripsRouter/TripsRouter.js
@@ -10,11 +10,12 @@ TripsRouter
         const {
             pick_up_address,
             drop_off_address,
-            zip_code,
+            pick_up_zip_code,
             pick_up_lat,
             pick_up_lng,
             drop_off_lat,
             drop_off_lng,
+            drop_off_zip_code,
             distance,
             duration,
             price,
@@ -27,11 +28,12 @@ TripsRouter
         const newTrip = {
             pick_up_address,
             drop_off_address,
-            zip_code,
+            pick_up_zip_code,
             pick_up_lat,
             pick_up_lng,
             drop_off_lat,
             drop_off_lng,
+            drop_off_zip_code,
             distance,
             duration,
             price,
@@ -58,6 +60,101 @@ TripsRouter
                     createdTrip
                 });
             });
+    })
+
+TripsRouter
+    .route("/trip/:id")
+    .all(requireAuth)
+    .get((req, res)=>{
+        const dataBase = req.app.get("db");
+
+        TripServices.getTripById(dataBase, req.params.id)
+            .then( trip => {
+                if(!trip){
+                    return res.status(404).json({
+                        error: "Trip not not found"
+                    });
+                };
+
+                return res.status(200).json({
+                    trip
+                });
+            });
+    })
+    .patch((req, res)=>{
+        const {
+            pick_up_address,
+            drop_off_address,
+            zip_code,
+            pick_up_lat,
+            pick_up_lng,
+            drop_off_lat,
+            drop_off_lng,
+            distance,
+            duration,
+            price,
+            scheduled,
+            scheduled_date_time,
+            time_requested,
+            date_created
+        } = req.body;
+
+        const updateTrip = {
+            pick_up_address,
+            drop_off_address,
+            zip_code,
+            pick_up_lat,
+            pick_up_lng,
+            drop_off_lat,
+            drop_off_lng,
+            distance,
+            duration,
+            price,
+            scheduled,
+            scheduled_date_time,
+            time_requested,
+            passenger_id: req.user.id,
+            date_created
+        };
+        const dataBase = req.app.get("db");
+        const id = req.params.id;
+
+        TripServices.getTripById(dataBase, id)
+            .then( dbTrip => {
+                if(!dbTrip){
+                    return res.status(404).json({
+                        error: `Trip not found`
+                    });
+                };
+
+                TripServices.updateTrip(dataBase, updateTrip, updateTrip.id)
+                    .then( updatedTrip => {
+                        return res.status(200).json({
+                            updatedTrip
+                        });
+                    });
+            });
+    })
+    .delete((req, res)=>{
+        const dataBase = req.app.get("db");
+        const id = req.params.id;
+
+        TripServices.getTripById(dataBase, id)
+            .then( trip => {
+                if(!trip){
+                    return res.status(404).json({
+                        error: "Trip not found"
+                    });
+                };
+
+                TripServices.deleteTrip(dataBase, id)
+                    .then( deletedTrip => {
+                        console.log("Line 153:", deletedTrip)
+                        return res.status(200).json({
+                            success: "Trip hous been deleted"
+                        });
+                    });
+            })
     });
 
 module.exports = TripsRouter;
--- src/services/DriversTripService/DriversTripService.js
+++ src/services/DriversTripService/DriversTripService.js
@@ -6,13 +6,12 @@ const DriversTripService = {
             trip_complete: false
         });
     },
-    updateDriverTripById(db, updateTrip, driverId, tripid){
+    updateDriverTripById(db, updateTrip, driverId, tripId){
         return db.update(updateTrip).from("trips").where({
             driver_id: driverId,
-            id: tripid
+            id: tripId
         }).returning("*").then(([updatedTrip]) => updatedTrip);
     }
-}
-
+};
 
 module.exports = DriversTripService;
